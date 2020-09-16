006de08068ca4fec9187f766b4080974965cc139
satyam-raikar <satyamraikar464@gmail.com>
2020-09-17 01:25:01 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
-import MyApp from "./MyApp";
+import App from "./components/App";
+import MyApp from "./components/MyApp";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
